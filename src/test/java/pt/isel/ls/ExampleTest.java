12f8c63cc0f63e6a7c05dbc36a8cf159bf663821
Pedro Felix <pmhsfelix@gmail.com>
2020-02-27 07:54:06 +0000

    adds java project structure

---
+++ src/test/java/pt/isel/ls/ExampleTest.java
@@ -0,0 +1,13 @@
+package pt.isel.ls;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+    @Test
+    public void example() {
+
+        assertEquals(2, 1 + 1);
+    }
+}
