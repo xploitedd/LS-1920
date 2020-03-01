b5cf7dd123def7fa59203f99a871fa8778c87663
Pedro Felix <pmhsfelix@gmail.com>
2020-03-01 10:34:25 +0000

    adds test examples from Friday's lecture

--- src/main/java/pt/isel/ls/App.java
+++ src/main/java/pt/isel/ls/App.java
@@ -2,6 +2,7 @@ package pt.isel.ls;
 
 public class App {
     public static void main(String[] args) {
+
         System.out.println("Hello LS");
     }
 }
--- src/test/java/pt/isel/ls/ExampleTest.java
+++ src/test/java/pt/isel/ls/ExampleTest.java
@@ -2,12 +2,29 @@ package pt.isel.ls;
 
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
+
     @Test
     public void example() {
+        // arrange | given
+        int a = 1;
+        int b = 2;
 
-        assertEquals(2, 1 + 1);
+        // act | when
+        int result = a + b;
+
+        // assert | then
+        assertEquals(3, result);
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void do_not_ignore_unexpected_exceptions_on_tests() throws FileNotFoundException {
+        // test methods can have a non-empty `throws` exception list.
+        new FileInputStream("does-not-exist");
     }
 }
