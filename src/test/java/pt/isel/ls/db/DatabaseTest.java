96a09a042744d448c018cd0dac7be46eb27db71f
Ricardo Margalhau <kryfor@outlook.com>
2020-03-05 21:49:04 +0000

    fixes some tests, adds more

--- src/test/java/pt/isel/ls/db/DatabaseTest.java
+++ src/test/java/pt/isel/ls/db/DatabaseTest.java
@@ -3,7 +3,6 @@ package pt.isel.ls.db;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import javax.xml.crypto.Data;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -29,7 +28,7 @@ public class DatabaseTest {
     }
 
     @BeforeClass
-    public static void databaseTests_createSchema() throws IOException, SQLException {
+    public static void databaseTests_createSchema() throws SQLException, IOException {
         executeFile("src/test/sql/createSchema.sql");
         executeFile("src/test/sql/addData.sql");
     }
@@ -37,32 +36,38 @@ public class DatabaseTest {
     @Test
     public void databaseTests_insert() throws SQLException {
         Connection conn = Database.getInstance().getConnection();
-        assertEquals(1,conn.prepareStatement("INSERT INTO courses(name) VALUES ('LEIM');").executeUpdate());
+        assertEquals(1, conn.prepareStatement("INSERT INTO courses(name) VALUES ('LEIM');").executeUpdate());
         conn.close();
     }
 
     @Test
     public void databaseTests_delete() throws SQLException {
         Connection conn = Database.getInstance().getConnection();
-        assertEquals(1,conn.prepareStatement("DELETE FROM students WHERE name LIKE 'A%'").executeUpdate());
+        assertEquals(1, conn.prepareStatement("DELETE FROM students WHERE name LIKE 'A%'").executeUpdate());
         conn.close();
     }
 
     @Test
-    public void databaseTests_select() throws SQLException{
+    public void databaseTests_select() throws SQLException {
         Connection conn = Database.getInstance().getConnection();
         PreparedStatement stmt = conn.prepareStatement("SELECT * FROM students");
         ResultSet rst = stmt.executeQuery();
-        assertTrue(rst != null);
+
+        // check if there's 3 columns
+        assertEquals(3, rst.getMetaData().getColumnCount());
+
+        // check there is at least one student
+        assertTrue(rst.next());
+
         conn.close();
     }
 
     @Test
-    public void databaseTests_update() throws SQLException{
+    public void databaseTests_update() throws SQLException {
         Connection conn = Database.getInstance().getConnection();
         PreparedStatement stmt = conn.prepareStatement("UPDATE students SET name = ? WHERE number = ?");
-        stmt.setString(1,"Joana");
-        stmt.setInt(2,12345);
-        assertEquals(0,stmt.executeUpdate());
+        stmt.setString(1, "Joana");
+        stmt.setInt(2, 12345);
+        assertEquals(0, stmt.executeUpdate());
     }
 }
