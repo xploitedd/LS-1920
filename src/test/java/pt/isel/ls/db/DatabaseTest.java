2887c6c74a29d7749c2aaa2e5bc993aff4907891
Binary <binary.enigm01100001@gmail.com>
2020-03-05 19:06:49 +0000

    Added INSERT and DELETE tests

--- src/test/java/pt/isel/ls/db/DatabaseTest.java
+++ src/test/java/pt/isel/ls/db/DatabaseTest.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import static org.junit.Assert.assertEquals;
+
 public class DatabaseTest {
 
     private static void executeFile(String filePath) throws SQLException, IOException {
@@ -25,11 +27,21 @@ public class DatabaseTest {
     @BeforeClass
     public static void databaseTests_createSchema() throws IOException, SQLException {
         executeFile("src/test/sql/createSchema.sql");
+        executeFile("src/test/sql/addData.sql");
     }
 
     @Test
     public void databaseTests_insert() throws SQLException, IOException {
-        executeFile("src/test/sql/addData.sql");
+        Connection conn = Database.getInstance().getConnection();
+        assertEquals(1,conn.prepareStatement("INSERT INTO courses(name) VALUES ('LEIM');").executeUpdate());
+        conn.close();
+    }
+
+    @Test
+    public void databaseTests_delete() throws SQLException {
+        Connection conn = Database.getInstance().getConnection();
+        assertEquals(1,conn.prepareStatement("DELETE FROM students WHERE name LIKE 'A%'").executeUpdate());
+        conn.close();
     }
 
 }
