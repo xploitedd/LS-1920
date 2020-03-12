0d042c45386aebbfb375a7a1e1b8aa4d9faa90cc
Hugo Dias <hugomdias.st@gmail.com>
2020-03-12 13:43:00 +0000

    Fixed CreateTables.sql

--- src/test/sql/CreateTables.sql
+++ src/test/sql/CreateTables.sql
@@ -1,13 +1,27 @@
-drop table if exists ROOMS;
+drop table if exists ROOM_LABEL;
 drop table if exists BOOKING;
+drop table if exists ROOMS;
 drop table if exists "user";
-drop table if exists LABEL;
+drop table if exists "label";
+
 
 CREATE TABLE ROOMS(
     rid SERIAL PRIMARY KEY,
     name varchar(50),
     location varchar(50),
     capacity smallint
+);
+
+CREATE TABLE "user"
+(
+    uid SERIAL PRIMARY KEY ,
+    email varchar(64) UNIQUE,
+    name varchar(50)
+);
+
+CREATE TABLE "label"(
+    lid SERIAL PRIMARY KEY,
+    name varchar(50) UNIQUE
 );
 
 CREATE TABLE BOOKING(
@@ -18,19 +32,8 @@ CREATE TABLE BOOKING(
     uid SERIAL REFERENCES "user"(uid)
 );
 
-CREATE TABLE "user"(
-    uid SERIAL PRIMARY KEY ,
-    email varchar(64) UNIQUE,
-    name varchar(50)
-);
-
-CREATE TABLE LABEL(
-    lid SERIAL PRIMARY KEY,
-    name varchar(50) UNIQUE
-);
-
 CREATE TABLE ROOM_LABEL(
     rlid SERIAL PRIMARY KEY ,
-    lid SERIAL REFERENCES LABEL(lid),
+    lid SERIAL REFERENCES "label"(lid),
     rid SERIAL REFERENCES ROOMS(rid)
 )
