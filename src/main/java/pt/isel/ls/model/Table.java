2a3cca72f8184d2340eb8598ce8e04daa1769969
Hugo Dias <hugomdias.st@gmail.com>
2020-03-13 18:48:08 +0000

    Added Table to model and TableView

---
+++ src/main/java/pt/isel/ls/model/Table.java
@@ -0,0 +1,17 @@
+package pt.isel.ls.model;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Set;
+
+public class Table {
+    private String[][] Columns;
+
+    public Table(int cSize, int rSize) {
+        Columns = new String[cSize][rSize];
+    }
+
+    public void addColumn(String columnName, LinkedList<String> values) {
+        return;
+    }
+}
