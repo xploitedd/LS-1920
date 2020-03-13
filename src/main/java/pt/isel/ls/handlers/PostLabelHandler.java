02e8c649a98982b5f0b0a48f4ea695af6727107a
Binary <binary.enigm01100001@gmail.com>
2020-03-13 17:50:58 +0000

    Did PostLabelHandler

--- src/main/java/pt/isel/ls/handlers/PostLabelHandler.java
+++ src/main/java/pt/isel/ls/handlers/PostLabelHandler.java
@@ -1,9 +1,11 @@
 package pt.isel.ls.handlers;
 
+import pt.isel.ls.router.RequestParameters;
 import pt.isel.ls.router.RouteRequest;
 import pt.isel.ls.router.RouteResponse;
 
 import javax.sql.DataSource;
+import java.sql.*;
 
 public class PostLabelHandler implements RouteHandler {
     private DataSource dataSource;
@@ -12,6 +14,19 @@ public class PostLabelHandler implements RouteHandler {
     }
     @Override
     public RouteResponse execute(RouteRequest request) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String n = request.getParameter("name").get(0);
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO label (name) VALUES (?);");
+            stmt.setString(1,n);
+            stmt.execute();
+            PreparedStatement ret = conn.prepareStatement("SELECT lid FROM label WHERE name = ?;");
+            stmt.setString(1,n);
+            ResultSet rs = stmt.executeQuery();
+            int lid = rs.getInt("lid"); //TODO: Return this somehow
+
+            return null;
+        } catch (RequestParameters.ParameterNotFoundException | SQLException e) {
+            return new RouteResponse().setException(e);
+        }
     }
 }
