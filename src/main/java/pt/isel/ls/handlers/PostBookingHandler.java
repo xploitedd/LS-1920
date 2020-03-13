734b21e538eecaf8016e4c942c1c63aee42e63c2
Binary <binary.enigm01100001@gmail.com>
2020-03-13 17:23:58 +0000

    Did PostBookingHandler

--- src/main/java/pt/isel/ls/handlers/PostBookingHandler.java
+++ src/main/java/pt/isel/ls/handlers/PostBookingHandler.java
@@ -1,13 +1,11 @@
 package pt.isel.ls.handlers;
 
+import pt.isel.ls.router.RequestParameters;
 import pt.isel.ls.router.RouteRequest;
 import pt.isel.ls.router.RouteResponse;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class PostBookingHandler implements RouteHandler {
     private DataSource dataSource;
@@ -16,11 +14,25 @@ public class PostBookingHandler implements RouteHandler {
     }
 
     @Override
-    public RouteResponse execute(RouteRequest request) throws SQLException {
-        Connection conn = dataSource.getConnection();
-        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM ROOM WHERE rid IN (SELECT rid FROM ROOM_LABEL WHERE lid = ?)");
-        ResultSet res = stmt.executeQuery();
-        conn.close();
-        return null;
+    public RouteResponse execute(RouteRequest request) {
+        try (Connection conn = dataSource.getConnection()) {
+            int rid = Integer.parseInt(request.getPathParameter("rid"));
+            int uid = Integer.parseInt(request.getPathParameter("rid"));
+            String begin = request.getParameter("begin").get(0);
+            String end = request.getParameter("end").get(0);
+            //TODO: Make sure these Strings are TIMESTAMPs
+            Timestamp b = Timestamp.valueOf(begin);
+            Timestamp e = Timestamp.valueOf(end);
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO BOOKING (begin,\"end\",rid,uid) VALUES (?,?,?,?);");
+            stmt.setTimestamp(1,b);
+            stmt.setTimestamp(2,e);
+            stmt.setInt(3,rid);
+            stmt.setInt(4,uid);
+
+            ResultSet res = stmt.executeQuery();
+            return null;
+        } catch (RequestParameters.ParameterNotFoundException | SQLException e) {
+            return new RouteResponse().setException(e);
+        }
     }
 }
