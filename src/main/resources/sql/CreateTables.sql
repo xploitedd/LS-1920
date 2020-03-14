d5d4ad35b75b2d71ae8364109212bcabf327b190
Ricardo Margalhau <kryfor@outlook.com>
2020-03-14 20:17:32 +0000

    adds relational integrity checks

--- src/main/resources/sql/CreateTables.sql
+++ src/main/resources/sql/CreateTables.sql
@@ -1,39 +1,41 @@
-drop table if exists ROOM_LABEL;
-drop table if exists BOOKING;
-drop table if exists ROOMS;
+drop table if exists room_label;
+drop table if exists booking;
+drop table if exists room;
 drop table if exists "user";
 drop table if exists "label";
 
-
-CREATE TABLE ROOMS(
+CREATE TABLE room (
     rid SERIAL PRIMARY KEY,
     name varchar(50),
     location varchar(50),
-    capacity smallint
+    description varchar(200),
+    capacity smallint,
+    constraint room_capacity_min_check check (capacity > 1)
 );
 
-CREATE TABLE "user"
-(
-    uid SERIAL PRIMARY KEY ,
+CREATE TABLE "user" (
+    uid SERIAL PRIMARY KEY,
     email varchar(64) UNIQUE,
     name varchar(50)
 );
 
-CREATE TABLE "label"(
+CREATE TABLE "label" (
     lid SERIAL PRIMARY KEY,
     name varchar(50) UNIQUE
 );
 
-CREATE TABLE BOOKING(
-    bid SERIAL PRIMARY KEY ,
+CREATE TABLE booking (
+    bid SERIAL PRIMARY KEY,
     begin TIMESTAMP,
     "end" TIMESTAMP,
-    rid SERIAL REFERENCES ROOMS(rid),
-    uid SERIAL REFERENCES "user"(uid)
+    rid SERIAL REFERENCES room(rid),
+    uid SERIAL REFERENCES "user"(uid),
+    constraint booking_begin_end_diff_check check ((extract(epoch from "end"-begin) / 60) >= 10),
+    constraint booking_begin_minute_check check (cast(extract(minute from begin) as decimal) % 10 = 0),
+    constraint booking_end_minute_check check (cast(extract(minute from "end") as decimal) % 10 = 0)
 );
 
-CREATE TABLE ROOM_LABEL(
-    rlid SERIAL PRIMARY KEY ,
+CREATE TABLE ROOM_LABEL (
     lid SERIAL REFERENCES "label"(lid),
-    rid SERIAL REFERENCES ROOMS(rid)
+    rid SERIAL REFERENCES room(rid)
 )
