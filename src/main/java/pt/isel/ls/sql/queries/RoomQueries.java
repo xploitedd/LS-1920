4393b249beee2816b92ef0dda8e9bc3bd60b58b5
Hugo Dias <hugomdias.st@gmail.com>
2020-03-19 13:59:16 +0000

    Finished RoomQueries (Untested)

--- src/main/java/pt/isel/ls/sql/queries/RoomQueries.java
+++ src/main/java/pt/isel/ls/sql/queries/RoomQueries.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 
 import pt.isel.ls.model.Room;
 
@@ -13,13 +15,55 @@ public class RoomQueries extends DatabaseQueries {
         super(conn);
     }
 
-    public Room createNewRoom(int rid, String name, String location, int capacity, String description, String label) throws Throwable {
+    public Room createNewRoom(int rid, String name, String location, int capacity, String description, Optional<List<String>> labels) throws Throwable {
+        List<Integer> lids = new LinkedList<>();
+        if (labels.isPresent()) { //Gets List of labels to associate with room
+            for (String lbl : labels.get()) {
+                PreparedStatement ls = conn.prepareStatement(
+                        "SELECT lid FROM label WHERE name = ?;"
+                );
 
-        return null;
+                ls.setString(1,lbl);
+                ResultSet rls = ls.executeQuery();
+                int lid = -1;
+                if (rls.next()) {
+                    rls.getInt("lid");
+                } else {
+                    throw new IllegalArgumentException("Label '" + lbl + "' not found, aborted creation of room");
+                }
+                lids.add(lid);
+            }
+        }
+
+        PreparedStatement stmt = conn.prepareStatement(
+                "INSERT INTO room (name, location, capacity) VALUES (?,?,?);"
+        );
+
+        stmt.setString(1,name);
+        stmt.setString(2,location);
+        stmt.setInt(3,capacity);
+        stmt.execute();
+
+        Room toReturn = getRoom(rid);
+
+        for (int lid : lids) {
+            //insert rid-lid pairs into ROOM_LABEL
+            PreparedStatement rl = conn.prepareStatement(
+                    "INSERT INTO room_label (lid,rid) VALUES (?,?);"
+            );
+
+            rl.setInt(1,lid);
+            rl.setInt(2,rid);
+            rl.execute();
+        }
+
+        return toReturn;
     }
 
     public Room getRoom(int rid) throws Throwable {
-        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM room WHERE rid = ?");
+        PreparedStatement stmt = conn.prepareStatement(
+                "SELECT * FROM room WHERE rid = ?;"
+        );
         stmt.setInt(1,rid);
 
         ResultSet rs = stmt.executeQuery();
@@ -34,7 +78,9 @@ public class RoomQueries extends DatabaseQueries {
     }
 
     public Iterable<Room> getRooms() throws Throwable {
-        PreparedStatement stmt = conn.prepareStatement("SELECT room.rid, name, location, capacity, description FROM room FULL JOIN description d on room.rid = d.rid");
+        PreparedStatement stmt = conn.prepareStatement(
+                "SELECT room.rid, name, location, capacity, description FROM room FULL JOIN description d on room.rid = d.rid;"
+        );
         ResultSet rs = stmt.executeQuery();
 
         LinkedList<Room> results = new LinkedList<>();
