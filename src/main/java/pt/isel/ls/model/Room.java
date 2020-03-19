b0fcb78f231ae8b11060e6d89e75085708381fd0
Hugo Dias <hugomdias.st@gmail.com>
2020-03-19 13:39:58 +0000

    Added Room to model;Added roomQueries

---
+++ src/main/java/pt/isel/ls/model/Room.java
@@ -0,0 +1,33 @@
+package pt.isel.ls.model;
+
+public class Room {
+
+    private final int rid;
+    private final String name;
+    private final String description;
+    private final int capacity;
+    private final String location;
+
+    public Room(int rid, String name, int capacity, String description, String location) {
+        this.rid = rid;
+        this.name = name;
+        this.capacity = capacity;
+        this.description = description;
+        this.location = location;
+    }
+
+    public int getRid() {
+        return rid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() { return capacity; }
+
+    public String getDescription() { return description; }
+
+    public String getLocation() { return location; }
+
+}
--- src/main/java/pt/isel/ls/sql/queries/RoomQueries.java
+++ src/main/java/pt/isel/ls/sql/queries/RoomQueries.java
@@ -1,7 +1,11 @@
 package pt.isel.ls.sql.queries;
 
 import java.sql.Connection;
-import pt.isel.ls.model.Model;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.LinkedList;
+
+import pt.isel.ls.model.Room;
 
 public class RoomQueries extends DatabaseQueries {
 
@@ -9,12 +13,41 @@ public class RoomQueries extends DatabaseQueries {
         super(conn);
     }
 
-    public Iterable<Model> createNewRoom(String labelName) throws Throwable {
+    public Room createNewRoom(int rid, String name, String location, int capacity, String description, String label) throws Throwable {
+
         return null;
     }
 
-    public Iterable<Model> getRoom() throws Throwable {
-        return null;
+    public Room getRoom(int rid) throws Throwable {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM room WHERE rid = ?");
+        stmt.setInt(1,rid);
+
+        ResultSet rs = stmt.executeQuery();
+        rs.next();
+        int id = rs.getInt("rid");
+        String name = rs.getString("name");
+        String location = rs.getString("location");
+        int capacity = rs.getInt("capacity");
+        String desc = rs.getString("description");
+
+        return new Room(id, name, capacity, desc, location);
+    }
+
+    public Iterable<Room> getRooms() throws Throwable {
+        PreparedStatement stmt = conn.prepareStatement("SELECT room.rid, name, location, capacity, description FROM room FULL JOIN description d on room.rid = d.rid");
+        ResultSet rs = stmt.executeQuery();
+
+        LinkedList<Room> results = new LinkedList<>();
+        while (rs.next()) {
+            int rid = rs.getInt("rid");
+            String name = rs.getString("name");
+            String location = rs.getString("location");
+            int capacity = rs.getInt("capacity");
+            String desc = rs.getString("description");
+            results.add(new Room(rid, name, capacity, desc, location));
+        }
+
+        return results;
     }
 
 }
