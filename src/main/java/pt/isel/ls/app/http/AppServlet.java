a12a81e7634a19224eee1857e0522be6190baa7a
Ricardo Margalhau <kryfor@outlook.com>
2020-05-22 18:55:31 +0100

    fixes for the checkstyle

--- src/main/java/pt/isel/ls/app/http/AppServlet.java
+++ src/main/java/pt/isel/ls/app/http/AppServlet.java
@@ -13,11 +13,9 @@ import pt.isel.ls.router.response.HandlerResponse;
 import pt.isel.ls.view.ViewHandler;
 import pt.isel.ls.view.ViewType;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
