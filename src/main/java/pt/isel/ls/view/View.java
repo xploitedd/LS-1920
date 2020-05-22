08793c31b5f649fdc5047a721ab2997bc6670f3d
Ricardo Margalhau <kryfor@outlook.com>
2020-05-23 00:02:33 +0100

    adds more comments

--- src/main/java/pt/isel/ls/view/View.java
+++ src/main/java/pt/isel/ls/view/View.java
@@ -22,15 +22,31 @@ public abstract class View {
     protected final String title;
     private final boolean homeLink;
 
+    /**
+     * Creates a new View
+     * @param title title of this view
+     */
     public View(String title) {
         this(title, true);
     }
 
+    /**
+     * Creates a new View
+     * @param title title of this view
+     * @param homeLink specifies whether this view has a link to
+     *                 the homepage in the HTML representation
+     */
     public View(String title, boolean homeLink) {
         this.title = title;
         this.homeLink = homeLink;
     }
 
+    /**
+     * Render this view
+     * @param handler view handler
+     * @param type render type
+     * @param writer writer where to render
+     */
     final void render(ViewHandler handler, ViewType type, PrintWriter writer) {
         if (type == ViewType.HTML) {
             renderHtml(handler, writer);
@@ -39,6 +55,11 @@ public abstract class View {
         }
     }
 
+    /**
+     * Render this view in HTML
+     * @param handler view handler
+     * @param writer HTML writer
+     */
     private void renderHtml(ViewHandler handler, PrintWriter writer) {
         try {
             Element body;
@@ -62,14 +83,28 @@ public abstract class View {
         }
     }
 
+    /**
+     * Get the HTML body for this View
+     * @param handler view handler
+     * @return HTML body content
+     */
     protected Node getHtmlBody(ViewHandler handler) {
         return p("No html representation available!");
     }
 
+    /**
+     * Render this View in plaintext
+     * @param handler view handler
+     * @param writer text writer
+     */
     protected void renderText(ViewHandler handler, PrintWriter writer) {
         writer.println("No text representation available!");
     }
 
+    /**
+     * Get the title of this view
+     * @return view title
+     */
     public String getTitle() {
         return title;
     }
--- src/main/java/pt/isel/ls/view/ViewHandler.java
+++ src/main/java/pt/isel/ls/view/ViewHandler.java
@@ -9,14 +9,33 @@ public class ViewHandler {
 
     private final Router router;
 
+    /**
+     * Creates a new View Handler
+     *
+     * View Handlers are responsible for rendering views and
+     * passing important information to them
+     * @param router Application Router
+     */
     public ViewHandler(Router router) {
         this.router = router;
     }
 
+    /**
+     * Render a specific view
+     * @param view view to be rendered
+     * @param type render type
+     * @param writer where to render
+     */
     public final void render(View view, ViewType type, PrintWriter writer) {
         view.render(this, type, writer);
     }
 
+    /**
+     * Get route path from handler class
+     * @param clazz handler class
+     * @param params path parameters
+     * @return route path
+     */
     public String route(Class<? extends RouteHandler> clazz, Object... params) {
         return router.route(clazz, params);
     }
